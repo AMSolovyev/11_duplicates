db7e98aeae406673a6fe077451e1d3a7e8311510
AMSolovyev <alexander.solovyev7@gmail.com>
2018-05-31 19:50:31 +0300

    I have added the script

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# Anti-Duplicator
+# Duplicate 
+The script  ```duplicates.py``` looks for duplacate-files. 
 
-[TODO. There will be project description]
+
+# How to use 
+
+You can run the scrip to use on Linux python or python3 like this:
+```
+python3 duplicates.py 
+
+There is not any parametrs on input 
+Input the dirname:     
+
+```
+On Windows you use similarly.
+
+You can duplicate-files like this:
+```
+/media/linux/01D36157DCE63600/Обучение 2014/CD-метод-матер-15-2014/08-Книги по СМК/Трек-1-23/Adobe Acrobat Reader v5.0/Resource/Font/PFM/_ABI____.PFM
+/media/linux/01D36157DCE63600/Обучение 2014/CD-метод-матер-15-2014/08-Книги по СМК/Трек-24-35/Adobe Acroba Reader v5.0/Resource/Font/PFM/_ABI____.PFM
+====================================================================================================
+/media/linux/01D36157DCE63600/Обучение 2014/CD-метод-матер-15-2014/08-Книги по СМК/Трек-1-23/Adobe Acrobat Reader v5.0/Reader/plug_ins/vdkhome/vdk10.syx
+/media/linux/01D36157DCE63600/Обучение 2014/CD-метод-матер-15-2014/08-Книги по СМК/Трек-24-35/Adobe Acroba Reader v5.0/Reader/plug_ins/vdkhome/vdk10.syx
+```
 
 # Project Goals
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+  The code is written for educational purposes. Training course for web-developers -
+[DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,57 @@
+import os
+from collections import defaultdict
+import sys
+
+
+def get_size_name_file_and_path_dict(path):
+    found_size_name_file_and_path_dict = defaultdict(list)
+    for root, _, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_size = os.path.getsize(file_path)
+            found_size_name_file_and_path_dict['{}_{}'.format(
+                file, file_size)].append(file_path)
+    return found_size_name_file_and_path_dict
+
+
+def find_duplicates(size_name_file_and_path_dict):
+    return [file_path
+            for file_path in size_name_file_and_path_dict.values()
+            if len(file_path) > 1]
+
+
+def input_dir_name_or_exit():
+    if len(sys.argv) == 1:
+        print('There is not any parametrs on input ')
+        work_dir = input("Input the dirname:     ")
+    else:
+        work_dir == sys.argv[1]
+        if os.path.isdir(work_dir) and os.path.exists(work_dir):
+            print('Рабочая дректория:  {}'.format(work_dir))
+            sys.exist(1)
+    if not os.path.isdir(work_dir):
+            print('Указанный путь {} не является директорией'.format(work_dir))
+            sys.exit(1)
+    if not os.path.exists(work_dir):
+            print('Пути к папке не существует:  '.format(work_dir))
+            sys.exit(1)
+
+    return work_dir
+
+
+def print_duplicates(duplicates):
+    if duplicates is None:
+        print('Дубликаты файлов не найдены ')
+    else:
+        print('Найдены дубликаты')
+        for path in duplicates:
+            print('=' * 100)
+            for path in path:
+                print(path)
+
 
 if __name__ == '__main__':
-    pass
+    dir_name = input_dir_name_or_exit()
+    size_name_file_and_path_dict = get_size_name_file_and_path_dict(dir_name)
+    duplicates = find_duplicates(size_name_file_and_path_dict)
+    print_duplicates(duplicates)
