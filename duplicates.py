976678717e4f54afcb51746352b7e092e7c83d84
AMSolovyev <alexander.solovyev7@gmail.com>
2018-06-03 17:11:57 +0300

    I have added input work_dit into console

--- duplicates.py
+++ duplicates.py
@@ -14,9 +14,9 @@ def get_size_name_file_and_path_dict(path):
     return found_size_name_file_and_path_dict
 
 
-def find_duplicates(size_name_file_and_path_dict):
-    return {key: value for key, value in size_name_file_and_path_dict.items()
-            if len(value) > 1}
+def get_duplicates(size_name_file_and_path_dict):
+    return {file_info: paths for file_info, paths in size_name_file_and_path_dict.items()
+            if len(file_info) > 1}
 
 
 def print_duplicates(duplicates):
@@ -24,20 +24,19 @@ def print_duplicates(duplicates):
         print('Дубликаты файлов не найдены ')
     else:
         print('Найдены дубликаты')
-        delimiter = '=' * 100
-        for path in duplicates:
+        delimiter = '=' * 80
+        for paths in duplicates:
             print(delimiter)
-            for path in path:
-                print(path)
+            for path in paths:
+               print(path)
 
 
 if __name__ == '__main__':
-    work_dir = input('Input the dirname:   ')
-    if len(sys.argv) == 1:
-        print('Рабочая директория:  {}'.format(work_dir))
-    else:
-        print('There is not any parametrs on input ')
-
+    if len(sys.argv) <2:
+        exit('The directory is not specifies')
+    work_dir = sys.argv[1]
+    if not os.path.isdir(work_dir):
+        exit('It is not a directory')
     size_name_file_and_path_dict = get_size_name_file_and_path_dict(work_dir)
-    duplicates = find_duplicates(size_name_file_and_path_dict)
+    duplicates = get_duplicates(size_name_file_and_path_dict)
     print_duplicates(duplicates)
